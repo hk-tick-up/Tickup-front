cf4ff71904568d46da5b8fdd4ce1800e3bf652d5
G0sim <angela07697@gmail.com>
2024-11-14 15:35:08 +0900

    게임방 선택 css 및 폰트 선정

---
+++ app/(routes)/SelectGameType/page.tsx
@@ -0,0 +1,12 @@
+// 게임방식 선택하는 페이지
+import React from 'react'
+import '../../css/GameRoom/root.css';
+
+export default function Component() {
+    return (
+        <h1 className="font-[Youth] bg-gray-100 flex-1 flex flex-col items-center justify-center px-6 space-y-8">
+            게임방식 선택
+        </h1>
+
+    )
+}
