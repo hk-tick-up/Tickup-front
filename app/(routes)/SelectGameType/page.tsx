ccc4465f2b58f83e53ec73f30994973db5ef6ded
G0sim <angela07697@gmail.com>
2024-11-14 16:41:43 +0900

    issues #9

--- app/(routes)/SelectGameType/page.tsx
+++ app/(routes)/SelectGameType/page.tsx
@@ -4,9 +4,22 @@ import '../../css/GameRoom/root.css';
 
 export default function Component() {
     return (
-        <h1 className="font-[Youth] bg-gray-100 flex-1 flex flex-col items-center justify-center px-6 space-y-8">
-            게임방식 선택
-        </h1>
+        // <h1 className="font-[Youth] bg-gray-100 flex-1 flex flex-col items-center justify-center px-6 space-y-8">
+        //     모의 투자 게임으로 <br /> 나만의 투자 시작하기!
+        // </h1>
+        <div className="bg-gray-100 flex-1 flex flex-col items-center py-5">
+            <div className="font-[Freesentation-9Black] text-2xl">
+                <p>모의 투자 게임으로</p>
+                <p>나만의 투자 시작하기!</p>
+            </div>
+            <div className="font-[Youth] py-8 custom-color-gray">
+                <p>원하는 게임 방식을 선택하면</p>
+                <p>모의 투자 게임이 시작돼요</p>
+            </div>
+            <div className="flex justify-center space-x-10">
+                
+            </div>
+        </div>
 
     )
 }
