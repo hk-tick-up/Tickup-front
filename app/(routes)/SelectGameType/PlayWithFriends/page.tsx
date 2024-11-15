5c779518080bb2ee02d2ddd1162f770500dc8e8e
G0sim <angela07697@gmail.com>
2024-11-15 14:28:15 +0900

    play with together completed

--- app/(routes)/SelectGameType/PlayWithFriends/page.tsx
+++ app/(routes)/SelectGameType/PlayWithFriends/page.tsx
@@ -13,22 +13,22 @@ export default function Component() {
                     <p>모의 투자 게임으로</p>
                     <p>나만의 투자 시작하기!</p>
                 </div>
-                <div className='font-[Youth] custom-color-gray middle-components-position w-full flex flex-col items-center space-y-5'>
-                    <div>
+                <div className='font-[Youth] custom-color-gray middle-components-position'>
+                    <div className='adjustment-position'>
                         <div className='flex items-center'><img src='../images/GameRoom/hands.png' className='pr-1 h-6' />
-                        <div className='pt-1 text-xl'>친구와 함께 플레이 해요</div></div>
+                        <div className='notice-text'>친구와 함께 플레이 해요</div></div>
                     </div>
                     <div>
                         <div className='flex space-x-3 items-center'>
                             <input placeholder='초대코드를 입력하세요'/>
-                            <div><button>들어가기</button></div>
+                            <div><button className='join-button'>입장</button></div>
                         </div>
                     </div>
                 </div>
-                <div className='font-[Youth] custom-color-gray'>
-                    <p>함께 하고 싶은 친구를 불러주세요</p>
+                <div className='font-[Youth] custom-color-gray share-box'>
+                    <p className='adjustment-position'>함께 하고 싶은 친구를 불러주세요!</p>
                     <div>
-                        <button>코드를 만들어 공유하기</button>
+                        <button className='share-button'>코드를 만들어 공유하기</button>
                     </div>
                 </div>
             </div>
